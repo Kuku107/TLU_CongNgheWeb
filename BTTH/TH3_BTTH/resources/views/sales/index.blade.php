a895b2ad95b48155ed7b848a6d4ef2f808595a6f
Kuku107 <leduc1078@gmail.com>
2024-12-12 15:17:44 +0700

    fix bootstrap link and update medicine name in sales table

--- BTTH/TH3_BTTH/resources/views/sales/index.blade.php
+++ BTTH/TH3_BTTH/resources/views/sales/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initialscale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Sales</title>
 </head>
 <body>
@@ -27,7 +27,7 @@
                         <h5 class="card-title">{{ $sale->id }}</h5>
                     </div>
                     <div class="card-body">
-                        <p class="card-text">Medicine: {{ $sale->medicine }}</p>
+                        <p class="card-text">Medicine: {{ $sale->medicine->name }}</p>
                         <p class="card-text">Quantity: {{ $sale->quantity }}</p>
                         <p class="card-text">Date: {{ $sale->sale_date }}</p>
                         <p class="card-text">Customer phone: {{ $sale->customer_phone }}</p>
@@ -52,5 +52,7 @@
         @endforeach
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
