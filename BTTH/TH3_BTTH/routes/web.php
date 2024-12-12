015fd5101a1f49f3ffee09e8e9b1f6eb1a013f7c
Kuku107 <leduc1078@gmail.com>
2024-12-12 14:52:47 +0700

    index for prac01

--- BTTH/TH3_BTTH/routes/web.php
+++ BTTH/TH3_BTTH/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MedicineController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/medicines", MedicineController::class . "@index")
+    -> name("medicines.index");
+
+Route::get("/sales", SaleController::class . "@index")
+    -> name("sales.index");
